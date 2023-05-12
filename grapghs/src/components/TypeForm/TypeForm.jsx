b4d67549709f3e9ac93e8f5a5e04db5c73cb11e6
SlavadevV <slava.dev.vodopyanov@gmail.com>
2023-05-12 12:26:45 +0300

    init

--- grapghs/src/components/TypeForm/TypeForm.jsx
+++ grapghs/src/components/TypeForm/TypeForm.jsx
@@ -4,13 +4,21 @@ import { useEffect } from 'react';
 
 const TypeForm = ({ setData }) => {
     useEffect(() => {
-        fetch(`https://api.typeform.com/forms/PJ1tgUNv/responses?included_response_ids=zeeji8jl0z7otiqzvzeexgymvj11w120`)
+        fetch(`https://api.typeform.com/forms/PJ1tgUNv/responses?included_response_ids=zeeji8jl0z7otiqzvzeexgymvj11w120`, {
+            headers: {
+                'authorization': 'bearer ' + 'tfp_C1AhRvvVmC45oJ3Th19ckvhtipLFvdukbFde2RmaQ7zi_e52zcqM7DMvc'
+            }
+        })
             .then((res) => res.json())
             .then((res) => setData(res.items[0].answers))
     }, [])
 
     const test = (payload) => {
-        fetch(`https://api.typeform.com/forms/PJ1tgUNv/responses?included_response_ids=${payload.responseId}`)
+        fetch(`https://api.typeform.com/forms/PJ1tgUNv/responses?included_response_ids=${payload.responseId}`, {
+            headers: {
+                'authorization': 'bearer ' + 'tfp_C1AhRvvVmC45oJ3Th19ckvhtipLFvdukbFde2RmaQ7zi_e52zcqM7DMvc'
+            }
+        })
             .then((res) => res.json())
             .then((res) => setData(res.items[0].answers))
     }
